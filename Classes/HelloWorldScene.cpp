c6e987aa4abff019196e6d33cca85bf5738354bf
Appms <amserik@hotmail.com>
2014-11-07 15:09:21 +0100

    Clean up.

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -8,8 +8,6 @@ USING_NS_CC;
 
 #define INPUT_DEADZONE  ( 0.24f * FLOAT(0x7FFF) )  // Default to 24% of the +/- 32767 range.   This is a reasonable default value but can be altered if needed.
 
-HelloWorld* mainLayer;
-
 DWORD dwResult;
 Sprite3D* boss;
 Sprite3D* boss1;
@@ -18,22 +16,10 @@ Sprite* point1;
 Sprite* point2;
 Sprite* pointend;
 
-float point1_x = 200.0f;
-float point1_y = 200.0f;
-
-float point2_x = 300.0f;
-float point2_y = 100.0f;
-
-float pointend_x = 400.0f;
-float pointend_y = 200.0f;
-
 bool paused = false;
 
 float cameraAngle = 34;
-float zoom = 500.0f;
-
-float rightThumbX = 0;
-float rightThumbY = 32767;
+float zoom = 1000.0f;
 
 Camera* camera;
 
@@ -49,10 +35,6 @@ int num_towers = 0;
 Point position1;
 Point position2;
 
-Sprite* point1;
-Sprite* point2;
-Sprite* pointend;
-
 float point1_x = 0;
 float point1_y = -300;
 
@@ -62,15 +44,10 @@ float point2_y = 1500;
 float pointend_x = 1800;
 float pointend_y = 300;
 
-bool paused = false;
-
 bool menuTurrets = false;
 
 bool leftTriggerPushed = false;
 bool leftShoulderPushed = false;
-
-float cameraAngle = 34;
-float zoom = 1000.0f;
 
 float rightThumbX = 0;
 float rightThumbY = 0;
@@ -78,20 +55,8 @@ float rightThumbY = 0;
 float coolDownMax = 0.5;
 float coolDownNow = coolDownMax;
 
-Camera* camera;
-
 Scene* HelloWorld::createScene()
 {
-<<<<<<< HEAD
-    // 'scene' is an autorelease object
-    auto scene = Scene::create();
-    
-    // 'layer' is an autorelease object
-    mainLayer = HelloWorld::create();
-
-    // add layer as a child to scene
-    scene->addChild(mainLayer);
-=======
 	// 'scene' is an autorelease object
 	auto scene = Scene::create();
 
@@ -100,7 +65,6 @@ Scene* HelloWorld::createScene()
 
 	// add layer as a child to scene
 	scene->addChild(layer);
->>>>>>> origin/AMS
 
 	// return the scene
 	return scene;
@@ -109,7 +73,6 @@ Scene* HelloWorld::createScene()
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-<<<<<<< HEAD
     //////////////////////////////
     // 1. super init first
     if ( !Layer::init() )
@@ -162,60 +125,6 @@ bool HelloWorld::init()
     // add the sprite as a child to this layer
 	sprite->setCameraMask(2);
     this->addChild(sprite, 0);
-=======
-	//////////////////////////////
-	// 1. super init first
-	if (!Layer::init())
-	{
-		return false;
-	}
-
-	Size visibleSize = Director::getInstance()->getVisibleSize();
-	Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
-	/////////////////////////////
-	// 2. add a menu item with "X" image, which is clicked to quit the program
-	//    you may modify it.
-
-	// add a "close" icon to exit the progress. it's an autorelease object
-	auto closeItem = MenuItemImage::create(
-		"CloseNormal.png",
-		"CloseSelected.png",
-		CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width / 2,
-		origin.y + closeItem->getContentSize().height / 2));
-
-	// create menu, it's an autorelease object
-	auto menu = Menu::create(closeItem, NULL);
-	menu->setPosition(Vec2::ZERO);
-	this->addChild(menu, 1);
-
-	/////////////////////////////
-	// 3. add your codes below...
-
-	// add a label shows "Hello World"
-	// create and initialize a label
-
-	auto label = LabelTTF::create("Die World", "Arial", 24);
-
-	// position the label on the center of the screen
-	label->setPosition(Vec2(origin.x + visibleSize.width / 2,
-		origin.y + visibleSize.height - label->getContentSize().height));
-
-	// add the label as a child to this layer
-	this->addChild(label, 1);
-
-	// add "HelloWorld" splash screen"
-	auto sprite = Sprite::create("HelloWorld.png");
-
-	// position the sprite on the center of the screen
-	sprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
-
-	// add the sprite as a child to this layer
-	this->addChild(sprite, 0);
-	sprite->setCameraMask(2);
->>>>>>> origin/AMS
 
 	auto floor = Sprite3D::create("plano2.obj", "sand.png");
 	floor->setScale(100);
@@ -227,23 +136,19 @@ bool HelloWorld::init()
 	this->addChild(floor, 0);
 	
 	boss = Sprite3D::create("boss.obj", "boss.png");
-<<<<<<< HEAD
+
 	boss->setPosition3D(Vec3(400, 300, 100));
 	boss->setRotation3D(Vec3(90, 0, 180));
 	boss->setScale(20);
 	boss->setCameraMask(2);
 	this->addChild(boss, 0);
-=======
+
 
 	//boss->setPosition3D(Vec3(visibleSize.width / 2 + (rand()%2) -1 * rand()%1 * visibleSize.width / 2, visibleSize.height / 2 + (rand()%2) -1 * rand()%1 * visibleSize.height / 2, rand()%1 * 300));
-
-	boss->setPosition3D(Vec3(400, 300, 0));
 
 	// LO GIRA A LA DERECHA
 	//boss->setRotation3D(Vec3(0,90,0));
 	boss->setRotation3D(Vec3(90, 0, 180));
->>>>>>> origin/AMS
-
 	
 	boss2 = Sprite3D::create("boss.obj", "boss.png");
 	boss2->setPosition3D(Vec3(-500, 0, 100));
@@ -320,15 +225,8 @@ bool HelloWorld::init()
 	pointend->setPosition(Vec2(pointend_x, pointend_y));
 	pointend->setCameraMask(2);
 	this->addChild(pointend, 0);
-    
-	
 
 	//now the bezier config declaration
-
-	
-
-
-	boss->setCameraMask(2);
 
 	boss1 = Sprite3D::create("boss.obj", "boss.png");
 
@@ -347,11 +245,6 @@ bool HelloWorld::init()
 	this->addChild(boss1, 0);
 
 	boss1->setCameraMask(2);
-
-	camera = Camera::createPerspective(60, visibleSize.width / visibleSize.height, 1, 2000);
-	camera->setCameraFlag(CameraFlag::USER1);
-	this->addChild(camera, 1);
-	camera->setRotation3D(Vec3(cameraAngle, 0, 0));
 
 	_eventDispatcher->addCustomEventListener("object_collision", [=](EventCustom* event){
 		float* elements = static_cast<float*>(event->getUserData());
@@ -434,8 +327,6 @@ void HelloWorld::update(float dt)
 		}
 
 		WORD wButtons = state.Gamepad.wButtons;
-
-<<<<<<< HEAD
 		
 		// COLOCAR TORRETA
 		if(state.Gamepad.bLeftTrigger > 50) {
@@ -459,7 +350,6 @@ void HelloWorld::update(float dt)
 		// CONTROL DE NAVE
 		boss->setPosition3D(boss->getPosition3D() + Vec3(state.Gamepad.sThumbLX/4000,state.Gamepad.sThumbLY/4000,0));
 		
-=======
 		// CONTROL DE NAVE
 
 		if (wButtons & XINPUT_GAMEPAD_A)
@@ -469,12 +359,10 @@ void HelloWorld::update(float dt)
 
 		//drawOBB->setPosition3D(drawOBB->getPosition3D() + Vec3(state.Gamepad.sThumbLX / 4000, state.Gamepad.sThumbLY / 4000, 0));
 
->>>>>>> origin/AMS
 		// ROTACION DE NAVE
 		if (state.Gamepad.sThumbRY != 0) rightThumbY = state.Gamepad.sThumbRY;
 		if (state.Gamepad.sThumbRX != 0) rightThumbX = state.Gamepad.sThumbRX;
 
-<<<<<<< HEAD
 		boss->setRotation3D(Vec3(90, 0, -90 - atan2(rightThumbY, rightThumbX)*360/(2*M_PI)));
 			
 
@@ -538,7 +426,6 @@ void HelloWorld::update(float dt)
 		if(wButtons & XINPUT_GAMEPAD_DPAD_DOWN) zoom += 5;
 		//camera->setPosition3D(Vec3(boss->getPositionX(), boss->getPositionY() - sin(cameraAngle*(2*M_PI)/360)*zoom, boss->getPositionZ() + cos(cameraAngle*(2*M_PI)/360)*zoom ));
 		
-=======
 		boss->setRotation3D(Vec3(90, 0, -90 - atan2(rightThumbY, rightThumbX) * 360 / (2 * M_PI)));
 
 		//drawOBB->setRotation3D(Vec3(0, 0, -90 - atan2(rightThumbY, rightThumbX) * 360 / (2 * M_PI)));
@@ -615,8 +502,8 @@ void HelloWorld::update(float dt)
 
 		}
 
->>>>>>> origin/AMS
 	}
+
 	else
 	{
 		// Controller is not connected 
